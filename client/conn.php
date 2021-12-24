9670bf7d2773194c4191c12cf03385961207f364
lautalaserna <lasernalauta@gmail.com>
2021-12-24 01:05:03 -0300

    Cosas

--- client/conn.php
+++ client/conn.php
@@ -1,20 +1,16 @@
 <?php
+    try {
+        $host = "localhost";
+        $dbname = "todo-list";
+        $user = "root";
+        $pw = "";
 
-try{
+        $conn = new PDO ("mysql:host=$host;
+        dbname=$dbname", $user, $pw);
+        $conn->setAttribute(PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION);
 
-    $host = "localhost";
-    $dbname = "todo-list";
-    $user = "root";
-    $pw = "";
-
-    $conn = new PDO ("mysql:host=$host;
-    dbname=$dbname", $user, $pw);
-    $conn->setAttribute(PDO::ATTR_ERRMODE,
-    PDO::ERRMODE_EXCEPTION);
-
-}catch(PDOException $e){
-    echo "error is: " . $e->getMessage();
-}
-
-
+    } catch(PDOException $e) {
+        echo "error is: " . $e->getMessage();
+    }
 ?>
